5c1dfdb8ae5aac0c9ab80d9e2aeb7de64f23f79f
root <madisononealburke@gmail.com>
2018-08-05 08:20:53 +0000

    help with installing fabric

---
+++ 0x0F-load_balancer/fabricinstall.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+# bash script to automate the installation of Fabric for Python3
+# sudo pip3 uninstall Fabric
+# sudo apt-get -y update
+#sudo apt-get install -y libffi-dev
+#sudo apt-get install -y libssl-dev
+#sudo apt-get install -y build-essential
+#sudo apt-get install -y python3.4-dev
+#sudo apt-get install -y libpython3-dev
+## apt-get install -y python-dev
+#sudo pip3 install pyparsing
+#sudo pip3 install appdirs
+#sudo pip3 install --upgrade setuptools
+## sudo pip install cryptography
+sudo pip install Fabric3
