cca37597d54724794d28fe7b6dfb57bab8fa63ed
ruby <343@holberton.com>
2018-09-13 02:34:35 +0000

    add comment puppet file

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,3 +1,4 @@
+# fix typo in wp-settings file
 exec { 'fix typo in php config':
   command => 'sed -i "s/phpp/php/g" /var/www/html/wp-settings.php',
   path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
